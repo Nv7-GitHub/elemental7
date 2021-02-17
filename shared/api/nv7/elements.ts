0884e633c2920a07f405c080327653fb739f0151
Nv7 <junk4Nv7@gmail.com>
2021-02-16 18:01:29 -0800

    Getall method, much faster

--- shared/api/nv7/elements.ts
+++ shared/api/nv7/elements.ts
@@ -1,7 +1,6 @@
 import {Element} from "./types";
 import {Elem, ElementalLoadingUi} from "../../elem";
 import { NV7ElementalAPI } from "./nv7";
-import { getFound } from "./savefile";
 
 export async function getElem(api: NV7ElementalAPI, id: string): Promise<Elem> {
   var elemData: Element = api.elemCache[id];
@@ -45,29 +44,10 @@ export async function getCombination(api: NV7ElementalAPI, elem1: string, elem2:
 }
 
 export async function downloadElems(api: NV7ElementalAPI, ui: ElementalLoadingUi) {
-  ui.status("Reading Elements", 0);
-  var i: any;
-  let elems = await api.cache.getAllElements();
-  ui.status("Reading Elements", 0.5);
-  for (i in elems) {
-    api.elemCache[elems[i].name] = elems[i];
-    ui.status("Reading Elements", 0.5+(i+1/elems.length/2));
-  }
-  ui.status("Loading Savefile", 0)
-  let found = await getFound(api);
-  ui.status("Downloading Elements", 0);
-  var id: string;
-  for (i in found) {
-    id = found[i];
-    ui.status("Downloading Elements", i/found.length);
-    if (!(id in api.elemCache)) {
-      var elemData: Element = await api.cache.get(id);
-      if (!elemData) {
-        var req = await fetch(api.prefix + "get_elem/" + encodeURIComponent(id));
-        elemData = await req.json();
-        await api.cache.store(elemData);
-      }
-      api.elemCache[id] = elemData
-    }
-  }
+  ui.status("Downloading Elements", 0)
+  var resp = await fetch(api.prefix + "get_all/" + api.uid);
+  ui.status("Downloading Elements", 0.5);
+  var dat: Element[] = await resp.json();
+
+  await api.cache.storeAll(dat, ui, "Saving Elements");
 }
