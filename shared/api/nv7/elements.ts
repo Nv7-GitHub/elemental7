c81bcb31ae4d90c54d3f40d2cdffab2308518d2b
Nv7 <junk4Nv7@gmail.com>
2021-02-17 11:05:46 -0800

    Dont re-download it every time

--- shared/api/nv7/elements.ts
+++ shared/api/nv7/elements.ts
@@ -1,6 +1,7 @@
 import {Element} from "./types";
 import {Elem, ElementalLoadingUi} from "../../elem";
 import { NV7ElementalAPI } from "./nv7";
+import { getFound } from "./savefile";
 
 export async function getElem(api: NV7ElementalAPI, id: string): Promise<Elem> {
   var elemData: Element = api.elemCache[id];
@@ -44,10 +45,30 @@ export async function getCombination(api: NV7ElementalAPI, elem1: string, elem2:
 }
 
 export async function downloadElems(api: NV7ElementalAPI, ui: ElementalLoadingUi) {
-  ui.status("Downloading Elements", 0)
-  var resp = await fetch(api.prefix + "get_all/" + api.uid);
-  ui.status("Downloading Elements", 0.5);
-  var dat: Element[] = await resp.json();
+  await loadElems(api, ui);
 
-  await api.cache.storeAll(dat, ui, "Saving Elements");
+  ui.status("Loading Savefile", 0)
+  let found = await getFound(api);
+  
+  if (((found.length+30) - Object.keys(api.elemCache).length) > 50) {
+    ui.status("Downloading Elements", 0)
+    var resp = await fetch(api.prefix + "get_all/" + api.uid);
+    ui.status("Downloading Elements", 0.5);
+    var dat: Element[] = await resp.json();
+    await api.cache.storeAll(dat, ui, "Saving Elements");
+
+    await loadElems(api, ui);
+  }
+}
+
+async function loadElems(api: NV7ElementalAPI, ui: ElementalLoadingUi): Promise<void> {
+  ui.status("Reading Elements", 0);
+  var i: any;
+  let elems = await api.cache.getAllElements();
+  ui.status("Reading Elements", 0.5);
+  for (i in elems) {
+    api.elemCache[elems[i].name] = elems[i];
+    ui.status("Reading Elements", 0.5+(i+1/elems.length/2));
+  }
+  return;
 }
